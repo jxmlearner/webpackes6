f9ed9af2556e3634e6b964c22868663f682d38a6
jxmlearner <410958040@qq.com>
2018-12-26 11:15:16 +0800

    welcome页和slide页面的title动画

--- src/index.js
+++ src/index.js
@@ -11,17 +11,35 @@ import './assets/stylus/index.styl'
 
 
 var swiper = new Swiper('.swiper-container',{
-    direction: 'vertical'
+    direction: 'vertical',
+    on: {
+        slideChangeTransitionEnd:function(swiper) {
+            let index = this.activeIndex
+            $('.swiper-slide').eq(index).addClass('animate').siblings().removeClass('animate');
+        }
+    }
 })
 
 $('.loading').on('animationend',function(){
-    $(this).fadeOut()
+    $(this).fadeOut(100,function(){
+        $('.welcome').addClass('animate')
+    })
 })
-$('.welcome').on('click',function(){
-    $(this).fadeOut()
-})
+// $('.welcome').on('click',function(){
+//     $(this).fadeOut()
+// })
 
 //页面加载完成之后等待一小会时间，为页面添加animate类
 setTimeout(()=> {
     $('.loading').addClass('animate')
 },1000)
+
+// page1的按钮长按
+$('.welcome .rotate-btn-box').on('longTap',function(){
+    $('.welcome .bear-box').css('animation','bearDisappear 1s forwards')   //动画的时候 最好用了animation就全部用animation,不要和transition混用
+    // 让上面的动画执行完成后再页面消失
+    setTimeout(()=> {
+        $('.welcome').fadeOut(1000)
+        $('.page1').addClass('animate')
+    },1000)    
+})
